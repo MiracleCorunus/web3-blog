952da4d4debc6581c57698ac245c556d2ad0fefe
MiracleCorunus <miraclegwon@gmail.com>
2022-11-12 06:31:31 -0600

    build the simple blog contract and testcases

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,12 @@
-require("@nomicfoundation/hardhat-toolbox");
+
+require("@nomiclabs/hardhat-waffle");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+  }
 };
